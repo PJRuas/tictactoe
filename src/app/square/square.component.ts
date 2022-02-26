063ea6f409157b9e1d203ac7ef398bf790627f8d
Pedro Ruas <pjruas123@gmail.com>
2022-02-26 16:59:57 -0300

    Development

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+      <button mat-raised-button *ngIf="!value">{{value}}</button>
+      <button mat-raised-button color="primary" *ngIf="value == 'X'">{{value}}</button>
+      <button mat-raised-button color="warn" *ngIf="value == 'O'">{{value}}</button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+})
+export class SquareComponent{
+
+  @Input() value: 'X' | 'O' | undefined;
+
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,24 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 export const environment = {
   production: false
 };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxU0IXe3EW9CHzSFAx-ex2oiNGGI2OIR0",
+  authDomain: "tictactoe-7344d.firebaseapp.com",
+  projectId: "tictactoe-7344d",
+  storageBucket: "tictactoe-7344d.appspot.com",
+  messagingSenderId: "171173738794",
+  appId: "1:171173738794:web:d7512fb5aaff9b30050653",
+  measurementId: "G-KVKYWN8WYY"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 /*
  * For easier debugging in development mode, you can import the following file
